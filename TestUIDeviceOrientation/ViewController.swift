62270254bee768cb47e6ab93e916b4e119ba4059
Aditya Ayyakad <aayyakad@butterflynetinc.com>
2019-04-15 08:37:45 -0400

    Added better observation

--- TestUIDeviceOrientation/ViewController.swift
+++ TestUIDeviceOrientation/ViewController.swift
@@ -4,25 +4,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    deinit {
+        guard let notification = notification else { return }
+        NotificationCenter.default.removeObserver(notification)
     }
 
-    override func viewDidLayoutSubviews() {
-        label.text = {
-            if UIDevice.current.orientation.isPortrait {
-                return "Portrait"
-            } else if UIDevice.current.orientation.isLandscape {
-                return "Landscape"
-            } else if UIDevice.current.orientation.isFlat {
-                return "Flat"
-            } else {
-                return "Unknown"
+    @IBOutlet weak var label: UILabel!
+
+    private var notification: NSObjectProtocol?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+        notification = NotificationCenter.default.addObserver(
+            forName: UIDevice.orientationDidChangeNotification,
+            object: nil,
+            queue: .main,
+            using: { [weak self] _ in
+                self?.label.text = {
+                    if UIDevice.current.orientation.isPortrait {
+                        return "Portrait"
+                    } else if UIDevice.current.orientation.isLandscape {
+                        return "Landscape"
+                    } else if UIDevice.current.orientation.isFlat {
+                        return "Flat"
+                    } else {
+                        return "Unknown"
+                    }
+                }()
             }
-        }()
+        )
     }
 
 }
