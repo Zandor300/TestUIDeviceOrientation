b5e23d6d5e31b1a195d216e230de84757c226d8e
Zandor Smith <info@zsinfo.nl>
2019-05-02 18:44:52 +0200

    Added isPortrait, isFlat and isLandscape labels.

--- TestUIDeviceOrientation/ViewController.swift
+++ TestUIDeviceOrientation/ViewController.swift
@@ -10,11 +10,16 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var orientationNameLabel: UILabel!
+    @IBOutlet weak var isPortraitLabel: UILabel!
+    @IBOutlet weak var isLandscapeLabel: UILabel!
+    @IBOutlet weak var isFlatLabel: UILabel!
 
     private var notification: NSObjectProtocol?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.updateIsLabels()
 
         // Do any additional setup after loading the view, typically from a nib.
         notification = NotificationCenter.default.addObserver(
@@ -33,8 +38,27 @@ class ViewController: UIViewController {
                         return "Unknown"
                     }
                 }()
+                self?.updateIsLabels()
             }
         )
+    }
+
+    var isPortrait: Bool {
+        return UIDevice.current.orientation.isPortrait
+    }
+
+    var isLandscape: Bool {
+        return UIDevice.current.orientation.isLandscape
+    }
+
+    var isFlat: Bool {
+        return UIDevice.current.orientation.isFlat
+    }
+
+    func updateIsLabels() {
+        self.isPortraitLabel.text = "isPortrait == " + (self.isPortrait ? "true" : "false")
+        self.isLandscapeLabel.text = "isLandscape == " + (self.isLandscape ? "true" : "false")
+        self.isFlatLabel.text = "isFlat == " + (self.isFlat ? "true" : "false")
     }
 
 }
